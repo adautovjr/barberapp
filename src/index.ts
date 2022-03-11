2e5851e850d936d32c4fcdb084c4d4cee84736d6
Adauto VJR <adautovjr@yahoo.com>
2022-03-11 04:39:54 -0300

    Added tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express"
+import { DEFAULT_VARIABLE } from "@config/index"
 
 const app = express();
 
 app.get("/", (request, response) => {
+    console.log(DEFAULT_VARIABLE)
     return response.json({ message: "Hello world" })
 })
 
