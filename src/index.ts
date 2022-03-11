e6bb3bd968fe25c06cea86d286c831e2fb695c19
Adauto VJR <adautovjr@yahoo.com>
2022-03-11 05:14:48 -0300

    Added eslint and jest

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express from "express"
-import { DEFAULT_VARIABLE } from "@config/index"
+import express from 'express'
+import { DEFAULT_VARIABLE } from '@config/index'
 
-const app = express();
+const app = express()
 
-app.get("/", (request, response) => {
-    console.log(DEFAULT_VARIABLE)
-    return response.json({ message: "Hello world" })
+app.get('/', (request, response) => {
+  console.log(DEFAULT_VARIABLE)
+  return response.json({ message: 'Hello world' })
 })
 
 app.listen(3000)
