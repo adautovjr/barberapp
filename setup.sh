8c5fc9f94516ecff40039a58910a6417b967f02e
adautovjr <26850257+adautovjr@users.noreply.github.com>
2022-05-04 11:59:30 -0300

    fix: Fixed setup.sh commands

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ git clone git@github.com:adautovjr/barberapp.git barber_backend/
 cd barber_backend/
 git checkout stag/backend
 npm install
-docker-compose up -d
-npx prisma migrate
+sudo docker-compose up -d
+npx prisma migrate dev
 cd ../barber_flutter/
 git checkout stag/mobile
 flutter pub get
